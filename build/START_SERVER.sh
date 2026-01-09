b3624a9e35b71d75cba5a8d97f3ae949372c05e7
kenny <0@kenny.cat>
2026-01-09 15:41:34 +0100

    Delete build directory

--- build/START_SERVER.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# Simple script to start the HTTP server
-# This serves the AirPlay Receiver Windows build for download
-
-echo "Starting AirPlay Receiver Build Server..."
-echo ""
-python3 serve.py
